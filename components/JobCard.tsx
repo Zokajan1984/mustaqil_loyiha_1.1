f5640943d61668f6e9ead3714a7d20f15a7080a0
Zokajan <zokirjon.astanov84@gmail.com>
2026-05-14 15:01:33 +0500

    JobCard.tsx yaratdim va type Job qo'shdim

---
+++ components/JobCard.tsx
@@ -0,0 +1,14 @@
+type Job = {
+  id: string;
+  title: string;
+  company: string;
+  locaion: string;
+  salary: string;
+  category: string;
+  description: string;
+  requirements: string[];
+};
+
+export default function JobCard() {
+  return <div>JobCard</div>;
+}
